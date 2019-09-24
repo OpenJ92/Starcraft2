261b42168c4a9b6f37d65fbe6ad81a4ccdd5f4cc
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-09-24 18:58:23 -0400

    class form pulled from replay.active_units. Look to introduce this feature to each other Explore/*.py

--- data/Explore/active_units.py
+++ data/Explore/active_units.py
@@ -6,43 +6,35 @@ Github: https://github.com/OpenJ92
 Description: 
 """
 
+from collections import OrderedDict
+
 class Active_Units():
     """Docstring for Active_Units. """
     # Class Attributes
     dct = {}
-    dct_ = {}
 
     def __init__(self, replay):
         """TODO: to be defined.
         :replay: TODO
         """
         self._replay = replay
-    
-    # We need to consider how to combine the following two functions into a singular
-    # dictionary whose exterior keys are "key_sets" and "type_names" with mappings to
-    # the both sub dictionaryies. inside key sets, we have a unique numeric ID and a 
-    # prototype dictioary with keys and value type information. Inside type_names, we have
-    # keys that match the numeric ids in the previous subdictionary and a list of the objects
-    # which subscribe to that form. We then seek to construct a python snippet which transforms
-    # these prototypical dictionaryies into class definitions and psql class definitions.
-    def get_unique(self):
+
+    def construct_dct(self):
         """TODO: Docstring for get_unique.
         """
         objects = self._replay.active_units.values()
 
-        for data in objects:
-            if data.name not in self.__class__.dct.keys():
-                self.__class__.dct[data.name] = {key: type(data) for key, data in vars(data).items()}
+        for data_ in objects:
+            if data_.name not in self.__class__.dct.keys():
+                dct_temp = {}
+                for key, data in vars(data_).items():
+                    if isinstance(data, OrderedDict):
+                        dct_temp[key] = {type(i):type(j) for i, j  in data.items()}
+                    elif isinstance(data, dict):
+                        dct_temp[key] = {type(i):type(j) for i, j  in data.items()}
+                    elif isinstance(data, list):
+                        dct_temp[key] = [type(sub) for sub in data]
+                    else:
+                        dct_temp[key] = type(data)
 
-    def get_unique_key_set(self):
-        """TODO: Docstring for get_unique_key_set.
-        """
-        self.get_unique()
-        unique_objects = self.__class__.dct
-        __import__('pdb').set_trace()
-        for key, data in unique_objects.items():
-            if data.keys() not in self.__class__.dct_:
-                self.__class__.dct_[data.keys()] = key
-            else:
-                self.__class__.dct_[data.keys()] += key
-
+                self.__class__.dct[data_.name] = dct_temp
